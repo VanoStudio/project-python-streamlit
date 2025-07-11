20849e0d905eb026a3c7d20a932f85e77f66916e
VanoStudio <91299723+VanoStudio@users.noreply.github.com>
2025-07-11 21:42:50 +0700

    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,22 @@
 import streamlit as st
 
-st.title("Tebak Khodam Anomali")
-st.markdown(f""" Ini adalah web untuk mencari khodam anomali brainrot yang selama ini diam-diam tinggal di dirimu.
+if "page" not in st.session_state:
+    st.session_state.page = "home"
 
-Cara kerjanya simpel:\n
-👉 Tinggal klik-klik jawab soal survei absurd,\n
-👻 Nanti muncul khodam aneh yang cocok sama energi chaotic-mu.""")
+def ganti_halaman(target):
+    st.session_state.page = target
 
-nama = st.text_input("Sebelum itu, masukkan nama mu dulu", placeholder="Contoh: Fuad")
+if st.session_state.page == "home":
+
+    st.title("Tebak Khodam Anomali")
+    st.markdown(f""" Ini adalah web untuk mencari khodam anomali brainrot yang selama ini diam-diam tinggal di dirimu.
+    Cara kerjanya simpel:\n
+    👉 Tinggal klik-klik jawab soal survei absurd,\n
+    👻 Nanti muncul khodam aneh yang cocok sama energi chaotic-mu.""")
+    nama = st.text_input("Sebelum itu, masukkan nama mu dulu", placeholder="Contoh: Fuad")
+
+    if st.button("Mulai Survei"):
+        ganti_halaman("halaman1")
+
+elif st.session_state.page == "halaman1":
+    st.title("halaman soal nya")
