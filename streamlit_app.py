48ae8ab864eb288f2f1ccb3519c523b2c6109c11
VanoStudio <91299723+VanoStudio@users.noreply.github.com>
2025-07-11 19:30:20 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Tebak Khodam Anomali")
+st.markdown(f""" Ini adalah web untuk mencari khodam anomali brainrot yang selama ini diam-diam tinggal di dirimu.
+
+Cara kerjanya simpel:\n
+👉 Tinggal klik-klik jawab soal survei absurd,\n
+👻 Nanti muncul khodam aneh yang cocok sama energi chaotic-mu.""")
+
+nama = st.text_input("Sebelum itu, masukkan nama mu dulu", placeholder="Contoh: Fuad")
